dfef4e5a632afdf67fce8b562602f556a0ffe12e
Sansão Vieira <sansao.vieira@global.ntt>
2024-05-11 00:54:34 -0300

    refactor imagem-certificado page

--- src/app/projects/imagem-certificado/page.tsx
+++ src/app/projects/imagem-certificado/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const ImagemCertificado = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center">
       <h1 className="text-center font-semibold text-3xl mb-4">Você ganhou um certificado!🤗</h1>
       <Image src="/certificate.png" alt="Certificado" width={800} height={250} />
     </div>
